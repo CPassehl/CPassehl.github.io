1cb1e96cb3b172012f0b5eb73183fa8d25b2ac0f
CPassehl <CPassehl@users.noreply.github.com>
2016-07-23 16:38:30 -0400

    Add files via upload

---
+++ movingCircles.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(1600, 1200);
+   
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, 100, 100);
+  } else {
+    fill(80, 115,196, 1);
+  }
+  ellipse(mouseX, mouseY, 60, 60);
+ 
+ 
+
+  if (keyIsPressed) {
+    ellipse(mouseX, mouseY, 300, 300);
+   
+} else {
+    fill(80, 115,196, 1);
+  }
+
+
+
+
+
+}
+
